f6724688cf8548c3980dc30a2d9210940ffd9400
sam-youll <sf4112@nyu.edu>
2024-03-12 12:52:09 -0500

    push

--- Assets/Resources/Sam/Scripts/Dummy.cs
+++ Assets/Resources/Sam/Scripts/Dummy.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Dummy : Tile
 {
+    protected TileText _tileText;
+    private int _lastHealth;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _lastHealth = health;
+        _tileText = GetComponent<TileText>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health < _lastHealth)
+        {
+            _lastHealth = health;
+
+            float roll = Random.value;
+            if (roll < .5f)
+            {
+                _tileText.DisplayText("Ouch!");
+            }
+            else
+            {
+                _tileText.DisplayText("Pain makes me feel gooooooood");
+            }
+
+        }
     }
 }
--- Assets/Resources/Sam/Scripts/mike.cs
+++ Assets/Resources/Sam/Scripts/mike.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class mike : apt283BasicEnemy
 {
+    protected TileText _tileText;
     private SpriteRenderer _sr;
     private float _flipTimer = 0;
     public override void init()
     {
         base.init();
         _sr = GetComponent<SpriteRenderer>();
+        _tileText = GetComponent<TileText>();
     }
     
     // Update is called once per frame
@@ -27,7 +29,9 @@ public class mike : apt283BasicEnemy
             {
                 _sr.flipX = true;
             }
-            _flipTimer = 0;
+            _flipTimer = Random.value/4;
+            
+            _tileText.DisplayText("bitch", 1f);
         }
     }
 }
